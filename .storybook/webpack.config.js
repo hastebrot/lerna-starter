76d3e4ea8c231f8bb108c0c8ef7af316fc36bef1
Benjamin Gudehus <hastebrot@gmail.com>
2019-06-29 03:31:13 +0200

    (fix) Let Storybook docs fail.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,19 +2,19 @@ const path = require("path")
 const createCompiler = require("@storybook/addon-docs/mdx-compiler-plugin")
 
 module.exports = async ({ config, mode }) => {
-  config.module.rules.push({
-    test: /\.mdx$/,
-    use: [
-      {
-        loader: "babel-loader",
-      },
-      {
-        loader: "@mdx-js/loader",
-        options: {
-          compilers: [createCompiler({})],
-        },
-      },
-    ],
-  })
+  // config.module.rules.push({
+  //   test: /\.mdx$/,
+  //   use: [
+  //     {
+  //       loader: "babel-loader",
+  //     },
+  //     {
+  //       loader: "@mdx-js/loader",
+  //       options: {
+  //         compilers: [createCompiler({})],
+  //       },
+  //     },
+  //   ],
+  // })
   return config
 }
